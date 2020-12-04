4a51ea17ec9e56cb4547bf11298fd13b83ab51fc
马中骏 <mazhongjun@mazhongjundeMacBook-Air.local>
2020-12-04 14:07:23 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+'''
+Author       : Ma Zhongjun
+Date         : 2020-12-04 14:05:15
+LastEditTime : 2020-12-04 14:07:06
+Description  : 根据AIS data寻找possible trip
+'''
+
+import os
+import sys
+
+import pandas as pd
+
+os.chdir(sys.path[0])
